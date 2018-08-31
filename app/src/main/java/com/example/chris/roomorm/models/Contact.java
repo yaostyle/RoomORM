878cbd0f799492bf5e21bac621fa137eb2b89287
chsu <yaostyle@gmail.com>
2018-08-30 22:12:44 -0700

    Basic layout

--- app/src/main/java/com/example/chris/roomorm/models/Contact.java
+++ app/src/main/java/com/example/chris/roomorm/models/Contact.java
@@ -1,8 +1,10 @@
-package com.example.chris.roomorm;
+package com.example.chris.roomorm.models;
 
 import android.arch.persistence.room.Entity;
 import android.arch.persistence.room.PrimaryKey;
 import android.support.annotation.NonNull;
+
+import java.util.Date;
 
 @Entity(tableName = "contact")
 public class Contact {
@@ -10,12 +12,17 @@ public class Contact {
     private String lastName;
     @PrimaryKey
     @NonNull
-
     private String phoneNumber;
+    private Date createDate;
 
     public String getFirstName() {
         return firstName;
     }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
 
     public String getLastName() {
         return lastName;
@@ -33,4 +40,14 @@ public class Contact {
     public void setPhoneNumber(@NonNull String phoneNumber) {
         this.phoneNumber = phoneNumber;
     }
+
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
+
+
 }
